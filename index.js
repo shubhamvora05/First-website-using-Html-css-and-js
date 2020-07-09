bde03f4b17e1d642ccc6c9f4dc81ab3510d363a5
shubhamvora05 <67896056+shubhamvora05@users.noreply.github.com>
2020-07-09 09:12:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// home page
+
+document.getElementById("nav-button").addEventListener("click", function(){
+  var img =document.getElementById("home-above");
+    img.classList.toggle("home-upperside-no-bg");
+});
+
+// contact page
