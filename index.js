1c849d38f3b0f224e6b3c5846ecdd786939868d0
shubhamvora05 <67896056+shubhamvora05@users.noreply.github.com>
2020-07-09 17:05:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// home page
+
+document.getElementById("nav-button").addEventListener("click", function(){
+  var img =document.getElementById("home-above");
+    img.classList.toggle("home-upperside-no-bg");
+});
+
+
+
+// contact page
+function validateForm() {
+  var name =  document.getElementById('name').value;
+  if (name == "") {
+       alert("name cannot be empty");
+      return false;
+  }
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+       alert( "Email cannot be empty");
+
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+        alert("Email format invalid");
+          return false;
+      }
+  }
+  var subject =  document.getElementById('subject').value;
+  if (subject == "") {
+      alert("Subject cannot be empty");
+      return false;
+  }
+  var message =  document.getElementById('message').value;
+  if (message == "") {
+  alert("Message cannot be empty");
+      return false;
+  }
+  alert("Sending...");
+}
