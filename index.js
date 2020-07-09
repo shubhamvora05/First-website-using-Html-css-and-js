14e4adaa0ee8f83b67afca3f910a330cf07dc57b
shubhamvora05 <67896056+shubhamvora05@users.noreply.github.com>
2020-07-09 17:04:50 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// home page
-
-document.getElementById("nav-button").addEventListener("click", function(){
-  var img =document.getElementById("home-above");
-    img.classList.toggle("home-upperside-no-bg");
-});
-
-// contact page
